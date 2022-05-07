550bead535b8d2fcc575ad728be42e5c6dfa54cf
Renan Souza de Oliveira <renansouzasccp@gmail.com>
2022-05-07 15:54:17 -0300

    Service layer, component registration

---
+++ src/main/java/com/renansouza/projetomavenspringjpa/services/UserService.java
@@ -0,0 +1,26 @@
+package com.renansouza.projetomavenspringjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renansouza.projetomavenspringjpa.entities.User;
+import com.renansouza.projetomavenspringjpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
