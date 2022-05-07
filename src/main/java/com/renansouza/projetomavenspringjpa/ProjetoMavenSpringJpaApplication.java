602a075bd2614dc80e598e1a8704748e3681c1db
Renan Souza de Oliveira <renansouzasccp@gmail.com>
2022-05-07 12:31:01 -0300

    Projeto Criado

---
+++ src/main/java/com/renansouza/projetomavenspringjpa/ProjetoMavenSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.renansouza.projetomavenspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMavenSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMavenSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renansouza/projetomavenspringjpa/ProjetoMavenSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renansouza.projetomavenspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMavenSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
