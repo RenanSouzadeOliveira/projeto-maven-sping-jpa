0c4d61dcf078f7018617d1307665a98d4ab86d44
Renan Souza de Oliveira <renansouzasccp@gmail.com>
2022-05-08 09:39:50 -0300

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/renansouza/projetomavenspringjpa/ProjetoMavenSpringJpaApplication.java
+++ src/main/java/com/renansouza/projetomavenspringjpa/ProjetoMavenSpringJpaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoMavenSpringJpaApplication {
-
+	// DIA 07/05 -> 3:23:13;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoMavenSpringJpaApplication.class, args);
 	}
