4b87c774af1fe35eee79c714ac9344ecd2a6b0d6
Renan Souza de Oliveira <renansouzasccp@gmail.com>
2022-05-08 11:45:00 -0300

    Subtotal & Total methods

--- src/main/java/com/renansouza/projetomavenspringjpa/entities/Payment.java
+++ src/main/java/com/renansouza/projetomavenspringjpa/entities/Payment.java
@@ -24,7 +24,8 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	
+
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
@@ -55,8 +56,7 @@ public class Payment implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
-	@JsonIgnore
+
 	public Order getOrder() {
 		return order;
 	}
