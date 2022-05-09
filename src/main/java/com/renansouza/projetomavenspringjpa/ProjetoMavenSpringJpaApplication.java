cb70dfcbf3779a6d2c55bdc3f91c260d4903dde3
Renan Souza de Oliveira <renansouzasccp@gmail.com>
2022-05-08 22:51:16 -0300

    Dev profile

--- src/main/java/com/renansouza/projetomavenspringjpa/ProjetoMavenSpringJpaApplication.java
+++ src/main/java/com/renansouza/projetomavenspringjpa/ProjetoMavenSpringJpaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoMavenSpringJpaApplication {
 	// DIA 07/05 -> 3:23:13;
+	// DIA 08/05 -> 2:29:33;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoMavenSpringJpaApplication.class, args);
 	}
